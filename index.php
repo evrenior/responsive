32a96aae2c8ae5a15831c81d873254672e539bcd
Dzianis Zhaunerchyk <evrenior@mail.ru>
2016-06-30 22:41:42 -0400

    New files from responsive

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Проба отзывчивой верстки</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	<div id="page">
+		<div class="blog section">
+			<h1 class="lede">
+				Почитать Блог Дениса 
+				<a href="#">
+					Далее...
+				</a>
+			</h1>
+			<div class="main">
+				<p  class="article">
+					<div class="header">
+						<div class="date">
+							10
+						</div>
+						<div class="title">
+							Aliquid illum tempora 
+						</div>
+					</div>
+					
+					Nostrum voluptatibus optio, autem dicta. Maiores, reprehenderit. 
+					Somewhere in my stomach I felt a cold, hard knot. Take stainless steel alloyed with titanium and plate it with three inches of lead. Take a brain made up of super-charged magnetic crystals enclosed in a leaden cranium and shielded by alloy steel. A bullet wouldn’t pierce it; radiations wouldn’t derange it; an axe wouldn’t break it.“Let’s go to town,” I said.They looked at me admiringly. With three hundred almost indestructible androids on the loose I was the big brave hero. I grinned at them and hoped they couldn’t see the sweat on my face. Then I walked over to the Copter and climbed in.“Coming?” I asked.Jack was pale under his freckles but Chief Dalton grinned back at me. “We’ll be right behind you, Morrison,” he said.
+					<div class="figure">
+						<p>
+							<img src="images/1.jpeg" alt="car" />
+							<b class="figcaption"> Где-то тут был робот.</b>	
+						</p>	
+					</div>
+					Animi inventore dolore illum ipsa? Behind me! So they could pick up the pieces. I gave them a cocky smile and switched on the engine, full speed.Carron City is about a mile from the plant. It has about fifty thousand inhabitants. At that moment, though, there wasn’t a soul in the streets. I heard people calling to each other inside their houses, but I didn’t see anyone, human or android. I circled in for a landing, the Police Copter hovering maybe a quarter of a mile back of me. Then, as the wheels touched, half a dozen androids came around the corner. They saw me and stopped, a couple of them backing off the way they had come. But the biggest of them turned and gave them some order that froze them in their tracks, and then he himself wheeled down toward me.
+
+				</p>	
+				<p  class="article">
+					<div class="header">
+						<div class="date">
+							9
+						</div>
+						<div class="title">
+							Quis impedit vero inventore facilis.
+						</div>
+					</div>
+					Ccusantium quia odit unde quae, veniam nam hic, veritatis praesentium 
+					doloremque minus! Et sit perspiciatis quam adipisci, at. Error qui officiis 
+					labore architecto mollitia! 
+				</p>	
+				<p  class="article">
+					<div class="header">
+						<div class="date">
+							8
+						</div>
+						<div class="title">
+							Quis illum, quos, harum architecto asperiores 
+						</div>
+					</div>
+					Iure sequi accusantium a deserunt dolor velit quod unde, iste doloribus tenetur quo totam 
+					doloremque quasi adipisci tempore magni fugiat non nostrum excepturi. 
+					Facere neque iusto, ipsum quis.	
+				</p>
+			</div> <!-- end main -->
+			<div class="other">
+				<p  class="recent-entries">
+					Name Last name
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+					Ex accusantium delectus numquam fugit quaerat neque quis, 
+					doloribus, quo laboriosam culpa nostrum deleniti ipsum dolores 
+					blanditiis quasi vitae eum dolor. 
+				</p>	
+				<p  class="recent-entries">
+					Pariatur.
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+					Earum numquam nam tempora. Eaque veritatis consequatur 
+					consequuntur, itaque. 
+				</p>	
+				<p  class="recent-entries">
+					Name Last name
+					Distinctio id veniam voluptates voluptatum, 
+					enim nisi maxime atque dolor quasi maiores eum reiciendis a incidunt 
+					perspiciatis cupiditate laborum accusantium libero ut itaque unde 
+					consequatur dignissimos odio error voluptatibus. 
+				</p>	
+			</div>	<!-- end other -->	
+		</div>
+	</div>	
+</body>
+</html>
